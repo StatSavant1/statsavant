30a97e1abd34e2beb9b12dd929d519d10cd08366
Kof Sports <kofsports@Kofs-Air.lan>
2025-12-29 11:50:29 -0500

    Switch NBA API to use materialized view

--- app/api/fetch-nba-stats/route.ts
+++ app/api/fetch-nba-stats/route.ts
@@ -13,7 +13,7 @@ export async function GET() {
     const supabase = getSupabaseAdmin();
 
     const { data, error } = await supabase
-      .from("nba_props_with_stats")
+      .from("nba_props_with_stats_mv")
       .select(`
         player,
         market,
